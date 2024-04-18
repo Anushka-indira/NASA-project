a1f2b1d4a8d61ac685761c01450c4b3478ac1858
Anushka Agrawal <anushkaa@indiratrade.com>
2024-04-18 11:24:43 +0530

    build

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -5,11 +5,11 @@ const { mongoDisconnect, mongoConnect } = require("../../services/mongo");
 describe("Launches API", () => {
   beforeAll(async () => {
     await mongoConnect();
-  },90000);
+  },900);
 
   afterAll(async ()=>{
     await mongoDisconnect();
-  },90000)
+  },900)
 
   describe("Test GET /launches", () => {
     test("it should respnd with 200 success", async () => {
